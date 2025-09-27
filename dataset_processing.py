99d43d55c19e160560e19cb989d14df9aa4f24bd
chris <cjwalmsley@clara.co.uk>
2025-09-27 18:03:24 +0100

    added cosine distance metric to testing notebook

--- dataset_processing.py
+++ dataset_processing.py
@@ -1,11 +1,13 @@
 import re
 from collections import Counter
 from unidecode import unidecode
-import tarfile
 import warnings
 import nltk
 from nltk.corpus import stopwords
 from datasets import load_dataset, load_from_disk
+from scipy.spatial.distance import cosine
+import ollama
+
 
 def load_squad_dataset(ds_filename="squad_dataset"):
     # check if dataset is available on disk, if not load it
@@ -13,40 +15,50 @@ def load_squad_dataset(ds_filename="squad_dataset"):
         print("Loading dataset from: " + ds_filename)
         ds = load_from_disk(ds_filename)
     except FileNotFoundError:
-        print("File not found, loading dataset from Huggingface and saving to: " + ds_filename)
+        print(
+            "File not found, loading dataset from Huggingface and saving to: "
+            + ds_filename
+        )
         ds = load_dataset("rajpurkar/squad")
         ds.save_to_disk(ds_filename)
     return ds
 
+
 def convert_stopwords_to_lower_case(a_string):
     try:
-        stopwords.words('english')
+        stopwords.words("english")
     except LookupError:
-        nltk.download('stopwords')
-    stop_words = set(stopwords.words('english'))
+        nltk.download("stopwords")
+    stop_words = set(stopwords.words("english"))
     words = a_string.split()
     # Convert stopwords to lowercase
     lower_case_stopwords = [word.lower() for word in stop_words]
     # Replace stopwords in the string with their lowercase versions
-    cleaned_string = ' '.join([word if word.lower() not in lower_case_stopwords else word.lower() for word in words])
+    cleaned_string = " ".join(
+        [
+            word if word.lower() not in lower_case_stopwords else word.lower()
+            for word in words
+        ]
+    )
     return cleaned_string
+
 
 def convert_first_character_to_lower_case_if_stopword(a_string):
     try:
-        stopwords.words('english')
+        stopwords.words("english")
     except LookupError:
-        nltk.download('stopwords')
-    stop_words = set(stopwords.words('english'))
+        nltk.download("stopwords")
+    stop_words = set(stopwords.words("english"))
     words = a_string.split()
     if words and words[0].lower() in stop_words:
         words[0] = words[0].lower()
-    cleaned_string = ' '.join(words)
+    cleaned_string = " ".join(words)
     return cleaned_string
 
 
 def remove_quotes(text):
     # remove " form the text
-    cleaned_text = text.replace('"', '')
+    cleaned_text = text.replace('"', "")
     return cleaned_text
 
 
@@ -62,7 +74,7 @@ def remove_quotes_from_file(filepath):
     return cleaned_filepath
 
 
-#move the ? from the end of each question to the start
+# move the ? from the end of each question to the start
 def move_question_mark_to_start(question, add_if_missing=True):
     if question.strip().endswith("?"):
         edited_question = "? " + question[:-1]
@@ -70,103 +82,145 @@ def move_question_mark_to_start(question, add_if_missing=True):
         if add_if_missing:
             edited_question = "? " + question
         else:
-            #raise an exception if the question does not end with a ?
+            # raise an exception if the question does not end with a ?
             raise ValueError(f"Question does not end with a question mark: {question}")
 
     return edited_question
 
+
 def any_word_match(row):
-    # Split both strings into sets of words for efficient lookup
+    # if the row contains a non-string value return False
+    if not isinstance(row["test_answer"], str) or not isinstance(
+        row["response_answer_formatted"], str
+    ):
+        return False
+    # return True if any word in test_answer is also in response_answer_formatted
     try:
-        stopwords.words('english')
+        stopwords.words("english")
     except LookupError:
-        nltk.download('stopwords')
-    stop_words = set(stopwords.words('english'))
+        nltk.download("stopwords")
+    stop_words = set(stopwords.words("english"))
     test_words = set(row["test_answer"].split())
-    response_words = set(row["response_answer"].split())
+    response_words = set(row["response_answer_formatted"].split())
     intersecting_words = test_words.intersection(response_words)
     open_class_intersecting_words = intersecting_words - stop_words
 
     return open_class_intersecting_words != set()
 
 
+def similarity_score(sentence_1, sentence_2):
+    # compare two sentences and return a similarity score based on their embedding representation
+    import ollama
+    from scipy.spatial.distance import cosine, euclidean
+
+    embedding_1 = ollama.embed(
+        model="embeddinggemma",
+        input=sentence_1,
+    ).embeddings[0]
+    embedding_2 = ollama.embed(
+        model="embeddinggemma",
+        input=sentence_2,
+    ).embeddings[0]
+
+    cosine_dist_12 = cosine(embedding_1, embedding_2)
+    euclidean_dist_12 = euclidean(embedding_1, embedding_2)
+
+    results_dict = {
+        "cosine_distance": cosine_dist_12,
+        "euclidean_distance": euclidean_dist_12,
+    }
+    return results_dict
+
+
 def replace_decimal_in_matched_string(matched_string):
-    #callable function to support the regex
+    # callable function to support the regex
     number_str = matched_string.group(0)
-    return number_str.replace('.', ' point ')
+    return number_str.replace(".", " point ")
+
 
 def truncate_to_max_words(text, max_words=20):
-	words = text.split()
-	if len(words) > max_words:
-		return ' '.join(words[:max_words])
-	else:
-		return text
+    words = text.split()
+    if len(words) > max_words:
+        return " ".join(words[:max_words])
+    else:
+        return text
+
 
 def convert_decimal_point_to_word(a_string):
-    #Replace "." with 'point' if it is part of a number.
+    # Replace "." with 'point' if it is part of a number.
     # Regex to find numbers with a decimal point:
     # \d+\.\d+  matches numbers like 1.23
     # \d+\.     matches numbers like 1. (dot at the end after digits)
     # \.\d+     matches numbers like .5 (dot at the beginning before digits)
     # The order matters to match \d+\.\d+ before \d+\. or \.\d+ for overlapping cases.
-    pattern = r'\d+\.\d+|\d+\.|\.\d+'
+    pattern = r"\d+\.\d+|\d+\.|\.\d+"
     return re.sub(pattern, replace_decimal_in_matched_string, a_string)
 
 
 def remove_full_stop(a_string):
-    #if the last character of a sentence is "." remove it
-    if a_string.strip().endswith('.'):
+    # if the last character of a sentence is "." remove it
+    if a_string.strip().endswith("."):
         return a_string[:-1]
     else:
         return a_string
 
 
 def replace_percent(a_string):
-    cleaned_text = re.sub(r"%", ' percent', a_string)
+    cleaned_text = re.sub(r"%", " percent", a_string)
     return cleaned_text
 
 
 def replace_dont_apostrophe(a_string):
-    cleaned_text = re.sub(r"don't", 'do not', a_string)
+    cleaned_text = re.sub(r"don't", "do not", a_string)
     return cleaned_text
 
 
 def remove_accents(text):
-    #Convert accented characters to unaccented ones
+    # Convert accented characters to unaccented ones
     text_unaccented = unidecode(text)
     return text_unaccented
 
 
-#remove all special characters except question marks and hyphen form the statements
+# remove all special characters except question marks and hyphen form the statements
 def remove_special_characters(text):
     """
     Removes special characters from a string, keeping alphanumeric characters and spaces.
     """
     # Keep only alphanumeric characters and spaces
-    cleaned_text = re.sub(r'[^A-Za-z0-9\s?-]+', '', text)
+    cleaned_text = re.sub(r"[^A-Za-z0-9\s?-]+", "", text)
     return cleaned_text
 
 
 def filter_by_max_words(the_df, max_words=10):
-    #returnes a new dataframe filtered such that each question, answer and statement has less than 11 words
+    # returnes a new dataframe filtered such that each question, answer and statement has less than 11 words
 
-    filtered_df = the_df[the_df.apply(
-        lambda row: len(row["question"].split()) <= max_words and len(row["answer"][0].split()) <= max_words and len(
-            row["statement"].split()) <= max_words, axis=1)]
+    filtered_df = the_df[
+        the_df.apply(
+            lambda row: len(row["question"].split()) <= max_words
+            and len(row["answer"][0].split()) <= max_words
+            and len(row["statement"].split()) <= max_words,
+            axis=1,
+        )
+    ]
     return filtered_df
 
 
 def clean_text(a_series, is_question):
-    #takes a dataframe series and applies reformatting
+    # takes a dataframe series and applies reformatting
     if is_question:
         a_series = a_series.apply(move_question_mark_to_start)
-    for function_name in (remove_full_stop, convert_decimal_point_to_word, remove_accents, remove_special_characters):
+    for function_name in (
+        remove_full_stop,
+        convert_decimal_point_to_word,
+        remove_accents,
+        remove_special_characters,
+    ):
         a_series = a_series.apply(function_name)
     return a_series
 
 
 def format_text(text, is_question=False):
-    #apply the formatting rules to the text
+    # apply the formatting rules to the text
     text = convert_first_character_to_lower_case_if_stopword(text)
     text = remove_full_stop(text)
     text = convert_decimal_point_to_word(text)
@@ -187,21 +241,23 @@ def is_pretraining_question(the_question, the_pretraining_questions):
 
 
 def write_training_file(a_series, a_filepath):
-    #write a file that can be used to train ANNABELL
+    # write a file that can be used to train ANNABELL
     with open(a_filepath, "w") as file:
         for statement in a_series:
             file.write(statement + "\n")
     print(f"file created: {a_filepath}")
 
 
-def write_testing_file(a_list, a_filepath):  #write a file that can be used to test ANNABELL
+def write_testing_file(
+    a_list, a_filepath
+):  # write a file that can be used to test ANNABELL
     with open(a_filepath, "w") as test_file:
         for question in a_list:
             test_file.write(f"{question}\n.x\n")
     print(f"file created: {a_filepath}")
 
 
-#produce a summary of a dataset by splits
+# produce a summary of a dataset by splits
 def dataset_summary(a_dataset):
     for split in a_dataset.keys():
         ds_split = a_dataset[split].to_pandas()
@@ -211,7 +267,11 @@ def dataset_summary(a_dataset):
         print("number of titles: " + str(len(set(titles))))
         print((set(titles)))
         bag_of_titles = Counter(titles)
-        print("titles with most numerous examples: " + str((bag_of_titles.most_common(20))) + "\n")
+        print(
+            "titles with most numerous examples: "
+            + str((bag_of_titles.most_common(20)))
+            + "\n"
+        )
 
 
 def data_frame_up_to_statement_title(a_dataframe, a_statemment):
@@ -219,11 +279,13 @@ def data_frame_up_to_statement_title(a_dataframe, a_statemment):
     row_index = row.index
     print(f"Row index of statement: '{a_statemment}': {row_index}")
     target_title = row["title"].values[0]
-    #find the index of the first row where the title is "Tuscon_Arizona"
+    # find the index of the first row where the title is "Tuscon_Arizona"
     title_index = a_dataframe[a_dataframe["title"] == target_title].index
-    print(f"Index of first row with title {target_title}: {title_index[0] if not title_index.empty else 'Not found'}")
-    #filter the dataset so that all the rows up to the title index are included
-    filtered_train_df = a_dataframe.iloc[:title_index[0]]
+    print(
+        f"Index of first row with title {target_title}: {title_index[0] if not title_index.empty else 'Not found'}"
+    )
+    # filter the dataset so that all the rows up to the title index are included
+    filtered_train_df = a_dataframe.iloc[: title_index[0]]
     print(f"Filtered DataFrame shape: {filtered_train_df.shape}")
     return filtered_train_df
 
@@ -234,7 +296,7 @@ def question_and_answer_pairs_from_log_file(test_log_filepath):
 
     question_and_answer_pairs = []
     for index, line in enumerate(test_log_lines):
-        if line.startswith("?"):
+        if line.startswith("?") or line.startswith(".stat") and index != 0:
             new_pair = [None, None]
             question = line.strip()
             new_pair[0] = question
@@ -250,7 +312,18 @@ def question_and_answer_pairs_from_log_file(test_log_filepath):
                 warnings.warn(f"Previous answer is None for question: {question}")
         else:
             continue
-    #print("length of log file questions: " + str(len(questions)))
-    #print("length of log file answers: " + str(len(answers)))
-    print("length of log file questions and answers: " + str(len(question_and_answer_pairs)))
     return question_and_answer_pairs
+
+
+def embedding_for_sentence(a_string):
+    embedding = ollama.embed(
+        model="embeddinggemma",
+        input=a_string,
+    ).embeddings[0]
+    return embedding
+
+
+def cosine_distance(a_row):
+    return cosine(
+        a_row["test_answer_embedding"], a_row["response_answer_formatted_embedding"]
+    )
