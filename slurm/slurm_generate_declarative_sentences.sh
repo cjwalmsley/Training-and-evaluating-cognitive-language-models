417c4ea39924fc2f2d55910c91e0450a46b9d816
chris <cjwalmsley@clara.co.uk>
2026-02-28 12:49:31 +0000

    update slurm script to python 3.13 venv

--- slurm/slurm_generate_declarative_sentences.sh
+++ slurm/slurm_generate_declarative_sentences.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --export=ALL
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate venv
+conda activate venv_python3.13
 
 # Define variables for the arguments, using provided parameters or defaults
 MODEL_STRING="${1:-llama3.2}"
