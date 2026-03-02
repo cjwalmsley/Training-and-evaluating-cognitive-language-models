4757ee8fa83f02f29392cd3663b45a4e827a1871
chris <cjwalmsley@clara.co.uk>
2026-03-02 14:30:29 +0000

    Added slurm script fro running ollama.

---
+++ slurm/slurm_run_ollama.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cjw81@kent.ac.uk
+#SBATCH --export=ALL
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:ampere:1
+#SBATCH --mem=32G
+
+# Run the command
+apptainer run --nv ollama.sif
+
