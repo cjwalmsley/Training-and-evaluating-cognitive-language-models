b0485386a3e08f8a1c1804d2c89f622ddacb01cf
chris <cjwalmsley@clara.co.uk>
2026-03-02 16:26:40 +0000

    Adding slurm script to train ANNABELL.

--- apptainer/README.md
+++ apptainer/README.md
@@ -6,3 +6,9 @@ apptainer build --build-arg ANNABELL_VERSION=annabell annabell.sif annabell.def
 needs to be run from the directory where annabell.sif is located
 apptainer shell -e --bind ./:/apptainer
 /Users/chris/PycharmProjects/Training-and-evaluating-cognitive-language-models/apptainer/annabell.sif
+
+#copy the sif to the cluster
+rsync -P ./annabell.sif cjw81@hydra.kent.ac.uk:
+
+#copy back to local machine (example)
+rsync -P cjw81@hydra.kent.ac.uk:~/Training-and-evaluating-cognitive-language-models/slurm/annabell.sif .
---
+++ slurm/slurm_train_annabel.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cjw81@kent.ac.uk
+#SBATCH --export=ALL
+#SBATCH --partition=cpu
+#SBATCH --gres=cpu:ampere:1
+#SBATCH --mem=8G
+
+#script to pretrain ANNABELL with language basics
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <logfile> <training_file> <pre-trained_weights>"
+    exit 1
+fi
+
+LOGFILE=$1
+TRAINING_FILE=$2
+PRETRAINED_WEIGHTS=$3
+
+# The time command's output (stderr) is appended to the log file.
+{ time (
+#turn on logging
+echo .logfile "$LOGFILE"
+#record the stats
+echo .stat
+#train using the commands provided in the file
+echo .f "$TRAINING_FILE"
+#save the weights
+echo .save "$PRETRAINED_WEIGHTS"
+#record the stats
+echo .stat
+# output timing data
+echo .t
+#turn off logging
+echo .logfile off
+#shut down ANNABELL
+echo .q
+) | annabell; } 2>> "$LOGFILE"
