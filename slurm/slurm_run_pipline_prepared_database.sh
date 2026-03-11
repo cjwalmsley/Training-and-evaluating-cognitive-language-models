7b0f194e9fd3b76ea1ae93883528d38527343717
chris <cjwalmsley@clara.co.uk>
2026-03-11 08:20:40 +0000

    Update to slurm script for running pipline

--- slurm/slurm_run_pipline_prepared_database.sh
+++ slurm/slurm_run_pipline_prepared_database.sh
@@ -3,8 +3,11 @@
 #SBATCH --mail-user=cjw81@kent.ac.uk
 #SBATCH --export=ALL
 #SBATCH --partition=cpu
-#SBATCH --gres=cpu:ampere:1
+#SBATCH --constraint=ampere
 #SBATCH --mem=16G
+
+
+#run with the following command: sbatch -p cpu --constraint=ampere --mem=16G ~/Training-and-evaluating-cognitive-language-models/slurm/slurm_run_pipline_prepared_database.sh
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate venv_python3.13
